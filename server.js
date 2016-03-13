e81dab820dbf6be4cd339add91b9b3f816d07915
Luuk <luukvoorbrowsen@gmail.com>
2016-03-13 15:58:50 +0000

    Setup complete!

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var express = require("express");
 var app = express();
 var server = http.createServer(app);
 
-app.use(express.static(path.resolve(__dirname, "client")));
-
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
