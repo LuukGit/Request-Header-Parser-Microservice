2cf3d63c0bbfbe46b395e5f53d7c11d15c7fc49d
Luuk <luukvoorbrowsen@gmail.com>
2016-03-13 20:28:42 +0000

    FINISHED

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/*  User Story: 
+      I can get the IP address, language and operating system for my browser.
+    
+    Example output:  
+      {"ipaddress":"143.177.24.100","language":"nl","software":"Windows NT 6.1; WOW64; rv:44.0"}
+*/
+
 var http = require('http');
 var path = require('path');
 var express = require("express");
@@ -5,8 +12,19 @@ var express = require("express");
 var app = express();
 var server = http.createServer(app);
 
+// Get some user info from the request header.
 app.get("/", function(req, res) {
-  res.send("Hello world!");
+  console.log(req.headers);
+  // Get the user's IP address
+  var ipaddress = req.headers["x-forwarded-for"];
+  
+  // Get the user's main language
+  var language = req.headers["accept-language"].split(",")[0];
+  
+  // Get information about the user's operating system.
+  var software = req.headers["user-agent"].match(/\((.*)\)/)[1];
+
+  res.send({ipaddress: ipaddress, language: language, software: software});
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
